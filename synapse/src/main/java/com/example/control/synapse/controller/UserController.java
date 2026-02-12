a20778159a997490bcf47559dcc01484d4fcad5f
Shriharsh Shreshtha <shriharshpvt@gmail.com>
2026-02-13 00:24:18 +0530

    built sign up (had to add a config folder to ensure log in/sign up before accesing any other endpoint

--- synapse/src/main/java/com/example/control/synapse/controller/UserController.java
+++ synapse/src/main/java/com/example/control/synapse/controller/UserController.java
@@ -1,5 +1,21 @@
-package com.example.control.synapse.controller;
+package com.example.control.synapse.controllers;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.control.synapse.dto.UserSignupRequest;
+import com.example.control.synapse.models.User;
+import com.example.control.synapse.services.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User signup(@RequestBody UserSignupRequest request) {
+        return userService.signup(request);
+    }
 }
