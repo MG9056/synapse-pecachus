f1f50b17ab58fe2b854abfae6dec755ba753bfaa
Shriharsh Shreshtha <shriharshpvt@gmail.com>
2026-02-12 19:05:17 +0530

    made repositories and started with controllers

--- synapse/src/main/java/com/example/control/synapse/controller/Restaurant.java
+++ synapse/src/main/java/com/example/control/synapse/controller/Restaurant.java
@@ -1,4 +1,10 @@
 package com.example.control.synapse.controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.example.control.synapse.service.RestaurantService;
+
+
+
 
 public class Restaurant {
     
