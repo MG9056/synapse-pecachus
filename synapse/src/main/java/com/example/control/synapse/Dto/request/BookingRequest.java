818078fdc9f2e6493de3a963d15d93ff918bc983
Shriharsh Shreshtha <shriharshpvt@gmail.com>
2026-02-13 17:46:21 +0530

    Added booking crud, and reserve seat is working

---
+++ synapse/src/main/java/com/example/control/synapse/Dto/request/BookingRequest.java
@@ -0,0 +1,32 @@
+package com.example.control.synapse.dto.request;
+
+public class BookingRequest {
+
+    private Long seatId;
+    private Long userId;
+    private Long eventId;
+
+    public Long getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(Long seatId) {
+        this.seatId = seatId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+}
