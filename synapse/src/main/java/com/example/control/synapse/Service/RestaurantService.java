d625a1a1078711a87b3328892aadf131702dbe27
Shriharsh Shreshtha <shriharshpvt@gmail.com>
2026-02-13 18:31:03 +0530

    added upload restaurant endpoint, routed via dto

--- synapse/src/main/java/com/example/control/synapse/Service/RestaurantService.java
+++ synapse/src/main/java/com/example/control/synapse/Service/RestaurantService.java
@@ -1,5 +1,36 @@
 package com.example.control.synapse.service;
 
+import com.example.control.synapse.models.Restaurant;
+import com.example.control.synapse.models.Stadium;
+import com.example.control.synapse.repository.StadiumRepository;
+
 public class RestaurantService {
+
+    public StadiumRepository stadiumRepository;
+
+    public RestaurantService(StadiumRepository stadiumRepository)
+    {this.stadiumRepository=stadiumRepository;
+
+    }
+
+    public String uploadRestaurant(String name, double rating, Long stadiumId)
+    {Restaurant restaurant= new Restaurant();
+
+    
+        restaurant.setName(name);
+        restaurant.setRating(rating);
+
+        Stadium stadium= stadiumRepository.findById(stadiumId)
+        .orElseThrow(() -> new RuntimeException("Stadium not found"));
+
+        restaurant.setStadiumId(stadium);
+
+    
+
+
+return "Restaurant uploaded!";
+
+
+    }
     
 }
--- synapse/src/main/java/com/example/control/synapse/controller/RestaurantController.java
+++ synapse/src/main/java/com/example/control/synapse/controller/RestaurantController.java
@@ -1,6 +1,12 @@
 package com.example.control.synapse.controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import com.example.control.synapse.dto.request.RestaurantRequest;
+import com.example.control.synapse.models.Food;
+import com.example.control.synapse.models.Restaurant;
+import com.example.control.synapse.repository.FoodRepository;
+import com.example.control.synapse.repository.RestaurantRepository;
 import com.example.control.synapse.service.RestaurantService;
 
 
@@ -10,10 +16,12 @@ import com.example.control.synapse.service.RestaurantService;
 public class RestaurantController {
     public final RestaurantRepository restaurantRepository;
     private final FoodRepository foodRepository;
+    private final RestaurantService restaurantService;
 
-    public RestaurantController(RestaurantRepository restaurantRepository, FoodRepository foodRepository)
+    public RestaurantController(RestaurantRepository restaurantRepository, FoodRepository foodRepository, RestaurantService restaurantService)
     {this.restaurantRepository= restaurantRepository;
         this.foodRepository = foodRepository;
+        this.restaurantService= restaurantService;
     }
 
     @GetMapping
@@ -26,13 +34,24 @@ public class RestaurantController {
     public Restaurant getRestaurantById(@PathVariable Long id)
 
     {
-        return restaurantRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Restaurant not found with id " + id));
+        return restaurantRepository.findById(id).orElseThrow();
     }    
 
     @GetMapping("{id}/menu")
     public List<Food> getFoodByRestaurantId(@PathVariable Long id)
     { return foodRepository.findByRestaurantId(id);
         
+    }
+
+    @PostMapping("/upload")
+    public String uploadRestaurant(@RequestBody RestaurantRequest restaurantRequest)
+    { return restaurantService.uploadRestaurant(
+        restaurantRequest.getName(),
+        restaurantRequest.getRating(),
+        restaurantRequest.getStadiumId()
+
+    );
+
     }
 
         
