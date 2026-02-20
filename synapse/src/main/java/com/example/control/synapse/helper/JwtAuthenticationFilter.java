7342753cb65282aad6da154e9a74ff4f2c7ba52e
Manomoy <manomoy.garg@gmail.com>
2026-02-20 18:29:52 +0530

    Bug fixing

--- synapse/src/main/java/com/example/control/synapse/helper/JwtAuthenticationFilter.java
+++ synapse/src/main/java/com/example/control/synapse/helper/JwtAuthenticationFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,13 +19,15 @@ import com.example.control.synapse.service.UserDetailsServiceImpl;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtils jwtUtils;
     private final UserDetailsServiceImpl userDetailsService;
-
+    public JwtAuthenticationFilter(JwtUtils jwtUtils,UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtils=jwtUtils;
+        this.userDetailsService=userDetailsService;
+    } 
     @Override
     protected void doFilterInternal(HttpServletRequest request, 
                                    HttpServletResponse response, 
