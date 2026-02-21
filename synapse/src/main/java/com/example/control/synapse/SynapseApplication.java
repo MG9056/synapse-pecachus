42b8bf5e113bfa89bb60028d17d805aded6cb0d2
Manomoy <manomoy.garg@gmail.com>
2026-02-21 14:30:56 +0530

    Added docker

--- synapse/src/main/java/com/example/control/synapse/SynapseApplication.java
+++ synapse/src/main/java/com/example/control/synapse/SynapseApplication.java
@@ -1,5 +1,9 @@
 package com.example.control.synapse;
 
+
+
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SynapseApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(SynapseApplication.class, args);
+		
 	}
 
 }
