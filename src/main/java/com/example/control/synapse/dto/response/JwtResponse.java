6f5167394fc1c2e99d1c6d936d813765e618790f
Manomoy <manomoy.garg@gmail.com>
2026-02-22 16:52:22 +0530

    Added firstname return on login

--- src/main/java/com/example/control/synapse/dto/response/JwtResponse.java
+++ src/main/java/com/example/control/synapse/dto/response/JwtResponse.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class JwtResponse {
     
     private String token;
+    private String firstName;
     private String type = "Bearer";
     private Long id;
     private String username;
